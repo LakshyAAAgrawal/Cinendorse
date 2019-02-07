4a7879b0440834dcd4d9e1fffa724eac2fcc0048
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2019-02-07 22:06:18 +0530

    Made a tree

--- data_collector.py
+++ data_collector.py
@@ -5,8 +5,7 @@ import pandas as pd
 import numpy as np
 from random import randint
 #client=MongoClient('mongodb://admin:mLabAdmin1000@ds119755.mlab.com:19755/try')
-#client=MongoClient('mongodb://admin:mLabAdmin1000@ds221405.mlab.com:21405/try')
-client=MongoClient()
+client=MongoClient('mongodb://admin:mLabAdmin1000@ds221405.mlab.com:21405/try')
 db=client['try']
 movies=db['movies']
 ratings=db['ratings']
@@ -30,69 +29,20 @@ def add_rating(movie_id, user_id, rating):
     global users, movies, ratings
     to_enter={'user_id':user_id, 'movie_id': movie_id, 'rating':rating}
     ratings.insert_one(to_enter)
-    update_ratings_average(user_id, true)
-    update_similarity_users(user_id)
+
+    ratings.find_one_and_update({'user_id':user_id, 'movie_id': movie_id}, {'$set': {'rating': float(rating)}}, upsert=True)
+
+    update_ratings_average(user_id)
+    #update_similarity_users(user_id)
 
 def update_ratings_average(user_id):
     global users, movies, ratings
     rating_cursor=ratings.find({'user_id':user_id})
     sum=0
-    for rating in ratings.find({'user_id':user_id}):
+    for rating in rating_cursor:
         sum=sum+rating['rating']
-    mean=sum/ratings.find({'user_id':user_id}).count()
+    mean=sum/rating_cursor.count()
     users.find_one_and_update({'_id': user_id}, {'$set': {'mean_rating': mean}})
-
-def update_ratings_movies(movie_id):
-    global users, movies, ratings
-    rating_cursor=ratings.find({'movie_id':movie_id})
-    sum=0
-    for rating in ratings.find({'movie_id':movie_id}):
-        sum=sum+rating['rating']
-    mean=sum/ratings.find({'movie_id':movie_id}).count()
-    movies.find_one_and_update({'_id': movie_id}, {'$set': {'mean_rating': mean}})
-
-
-def update_similarity_movies(movie_id):
-    global users, movies, ratings
-    movie_cursor=movies.find()
-    data_dict={}
-    # for  in movies.find():
-    #     data_dict[movie['_id']]={}
-    # for rating in ratings.find():
-    #     data_dict[rating['movie_id']][rating['user_id']]=rating['rating']
-    # #print(data_dict)
-    # df=pd.DataFrame(data_dict)
-    # print(df.mean(axis=1))
-    # df.to_csv('before.csv')
-    # ndf=df.sub(df.mean(axis=1), axis=0)
-    for movie in movies.find():
-        data_dict[movie['_id']]={}
-    for rating in ratings.find():
-        data_dict[rating['movie_id']][rating['user_id']]=rating['rating']
-    df=pd.DataFrame(data_dict)
-    ndf=df.sub(df.mean(axis=0), axis=1)
-    #might want to divide by standard deviation here!!
-
-    ndf=ndf.fillna(0)
-    print(ndf)
-    movie_1=ndf[movie_id].values
-    for movie in movies.find():
-        if movie['_id']==movie_id:
-            None
-        else:
-            movie_2=ndf[movie['_id']].values
-            dot_product=movie_1.dot(movie_2)
-            print('dot_product', dot_product)
-            norm_1=norm(movie_1)
-            norm_2=norm(movie_2)
-            if norm_1==0 or norm_2==0:
-                cosine=0
-            else:
-                cosine=dot_product/((norm_1)*(norm_2))
-            print('similarity between', movies.find({'_id':movie_id})[0]['title'], movie['title'], cosine)
-            movies.find_one_and_update({'_id': movie['_id']}, {'$set': {'similarity.'+movies.find_one({'_id':movie_id})['title']: float(cosine)}})
-            #print(user['username'])
-            movies.find_one_and_update({'_id': movie_id}, {'$set': {'similarity.'+str(movie['_id']): float(cosine)}})
 
 def update_similarity_users(user_id):
     global users, movies, ratings
@@ -123,25 +73,18 @@ def update_similarity_users(user_id):
         else:
             user_2=ndf[user['_id']].values
             dot_product=user_1.dot(user_2)
-            print('dot_product', dot_product)
             norm_1=norm(user_1)
             norm_2=norm(user_2)
             if norm_1==0 or norm_2==0:
                 cosine=0
             else:
                 cosine=dot_product/((norm_1)*(norm_2))
-            print('after cosine', cosine)
             users.find_one_and_update({'_id': user['_id']}, {'$set': {'similarity.'+users.find_one({'_id':user_id})['username']: cosine}})
-            print(user['username'])
             users.find_one_and_update({'_id': user_id}, {'$set': {'similarity.'+user['username']: float(cosine)}})
     return(ndf, df)
 
-def recommendations_user(n, username):
+def recommendations(n, user_id):
     global users, movies, ratings
-    if users.find({'username':username}).count()>0:
-        user_id=users.find({'username':username})[0]['_id']
-    else:
-        return(None)
     user_a=users.find_one({'_id':user_id})
     similarity_dict=user_a['similarity']
     users_to_consider=[]
@@ -150,9 +93,8 @@ def recommendations_user(n, username):
         if similarity_dict[user_2]>0:
             users_to_consider.append(user_2)
             for rating in ratings.find({'user_id':users.find_one({'username':user_2})['_id']}):
-                if not ([None, rating['movie_id']] in movies_to_consider):
+                if not (rating['movie_id'] in movies_to_consider):
                     movies_to_consider.append([None, rating['movie_id']])
-    to_ret=[]
     for movie_ls in movies_to_consider:
         i=0
 
@@ -184,16 +126,39 @@ def recommendations_user(n, username):
                 bias=bias+similarity*(ratings.find_one({'user_id':user_obj['_id'], 'movie_id':movie_id})['rating']-user_obj['mean_rating'])
             except:
                 None
-        #movie_ls[0]=bias/sum_weights + mean
-        to_ret.append([bias/sum_weights + mean, movie_ls[1]])
-    to_ret.sort(reverse=True)
-    return(to_ret[:n])
+        expectFalseed_rating=bias/sum_weights + mean
+        movie_ls[0]=bias/sum_weights + mean
+        print(movie_ls)
+    movies_to_consider.sort(reverse=True)
+    return(movies_to_consider[:15])
 
-def recom_parse(lis):
-    global users, movies, ratings
-    to_ret=[]
-    for i in lis:
-        to_ret.append(movies.find({'_id':i[1]})[0])
-    return(to_ret)
-#recommendation_list=recommendations(5,users.find().skip(randint(2,10)).next()['_id'])
-#print(recommendation_list)
+if __name__=='__main__':
+    error=True
+    while error:
+        try:
+            username=input('Enter username : ')
+            if username=='':
+                raise Exception()
+            if users.find({'username':username}).count()>0:
+                user_id=users.find({'username':username})[0]['_id']
+                error=False
+            else:
+                user_id=add_user(username, users)
+                error=False
+        except:
+            error=True
+    while True:
+        movie_obj=movies.find().skip(randint(0,4999)).next()
+        print(movie_obj['title'])
+        try:
+            in_s=input('Please enter rating, or blank for skip, c for exit : ')
+            rating=float(in_s)
+            if rating>0 and rating<5.1:
+                rating=in_s
+            else:
+                raise Exception('x should not exceed 5 or less than 0')
+        except:
+            if in_s=='c':
+                break
+        else:
+            add_rating(movie_obj['_id'], user_id, rating)
--- server/routes.py
+++ server/routes.py
@@ -2,7 +2,7 @@ from flask import render_template
 from server import app
 from flask import request
 from server.forms import LoginForm
-from server.movie_rec import *
+from Recommendation.movie_rec import *
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -12,7 +12,8 @@ def login():
     if request.method == 'POST':
       result = request.form
       recomm=recommendations_user(10, result['username'])
+
       if(recomm==None):
           return('No such user')
       else:
-          return(str(recomm))
+          return(str(recom_parse(recomm)))
