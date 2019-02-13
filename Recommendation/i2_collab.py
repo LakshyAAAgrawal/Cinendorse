c529366c5635460d10c4894ed1a374ebfb080c07
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2019-02-13 17:07:20 +0530

    Minor change in i2

--- Recommendation/i2_collab.py
+++ Recommendation/i2_collab.py
@@ -16,7 +16,7 @@ def norm(v):
     sum += v[i]**2
   return sum**(0.5)
 
-def rating_for(user_id, movie_id, algorithm='u2_collab', a=None):
+def rating_for(user_id, movie_id, arr_1, algorithm='u2_collab', a=None):
     if algorithm=='i2_collab':
         item_mean=np.nanmean(a.ratings_array, axis=0)[a.movie_index_dict[movie_id]]
         bias=0
@@ -24,7 +24,7 @@ def rating_for(user_id, movie_id, algorithm='u2_collab', a=None):
         for rating in a.ratings.find({'user_id':user_id, 'movie_id':{'$ne':movie_id}}, {'movie_id':1}):
             weight=a.movie_similarity_matrix[a.movie_index_dict[movie_id], a.movie_index_dict[rating['movie_id']]]
             if weight>0:
-                bias=bias+weight*(np.nan_to_num(a.ratings_array-np.nanmean(a.ratings_array, axis=0))[a.user_index_dict[user_id], a.movie_index_dict[rating['movie_id']]])
+                bias=bias+weight*(arr_1[a.user_index_dict[user_id], a.movie_index_dict[rating['movie_id']]])
                 sum_weights=sum_weights+abs(weight)
         if sum_weights!=0:
             exp_rating=item_mean+bias/sum_weights
@@ -34,7 +34,8 @@ def rating_for(user_id, movie_id, algorithm='u2_collab', a=None):
 
 def recommendations_for(n, user_id, algorithm='i2', a=None):
     rating_list=[]
+    arr_1=np.nan_to_num(a.ratings_array-np.nanmean(a.ratings_array, axis=0))
     for m_id in a.ratings.distinct('movie_id', {'user_id':{'$ne':user_id}}):
-        rating_list.append([rating_for(user_id, m_id, algorithm='i2_collab',a=a), m_id])
+        rating_list.append([rating_for(user_id, m_id, arr_1, algorithm='i2_collab',a=a), m_id])
     rating_list.sort(reverse=True)
     return(rating_list[:n])
